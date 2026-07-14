03029ba4186956ff7e59b4cd25c050a6e5b04e06
rifatalvi <rifatalvi765@gmail.com>
2026-07-15 05:11:24 +0600

    feat: add unsplash.com to allowed image domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "via.placeholder.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
